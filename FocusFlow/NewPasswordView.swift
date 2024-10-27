8b0865d6ea6ff840e3ae9656b45ebbcef0dd747d
Nevin Ozkan <ozkannevin73@gmail.com>
2024-10-28 00:10:58 +0300

    Created NewPasswordView

---
+++ FocusFlow/NewPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  NewPasswordView.swift
+//  FocusFlow
+//
+//  Created by Nevin Özkan on 27.10.2024.
+//
+
+import SwiftUI
+
+struct NewPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPasswordView()
+}
